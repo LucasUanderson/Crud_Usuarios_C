4db47f362e8afbd7f2a33632bbdb5088112be619
Lucas Uanderson <lucasuanderson159@live.com>
2022-11-08 22:46:00 -0300

    Create jquery.min.map

---
+++ TripcallC/TripcallC/Data/TripcallCContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using TripcallC.Models;
+
+namespace TripcallC.Data
+{
+    public class TripcallCContext : DbContext
+    {
+        public TripcallCContext (DbContextOptions<TripcallCContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<TripcallC.Models.UsuarioModel> UsuarioModel { get; set; } = default!;
+    }
+}
---
+++ TripcallC/TripcallC/Models/UsuarioModel.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace TripcallC.Models
+{
+    [Table("Usuario")]
+    public class UsuarioModel
+    {
+        [Display(Name = "Código")]
+        [Column("Id")]
+        public int Id { get; set; }
+
+        [Display(Name = "Name")]
+        [Column("Name")]
+        public string Name { get; set; }
+
+        [Display(Name = "Email")]
+        [Column("Email")]
+        public string Email { get; set; }
+
+        [Display(Name = "Senha")]
+        [Column("Senha")]
+        public string Senha { get; set; }
+
+
+    }
+}
